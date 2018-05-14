5de5ad6bb46eacdb5e49db7a30f5bc744d49f55a
Miguel Dias <m3k3r1@gmail.com>
2018-05-14 17:25:54 +0200

    RobotController implementation

--- src/application/RobotController.java
+++ src/application/RobotController.java
@@ -1,4 +1,59 @@
 package application;
 
-public class RobotController {
+import connection.ReceiverConnection;
+import connection.SenderConnection;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author User
+ */
+public class RobotController{
+
+    private DatagramSocket socketToStub;
+    private InetAddress stubAddress;
+    private byte[] buf;
+
+    private class Sender extends SenderConnection{
+
+        @Override
+        public void doSenderConnection(String nameHost) throws SocketException, UnknownHostException{
+
+            socketToStub = new DatagramSocket();
+            stubAddress = InetAddress.getByName("localhost");
+
+            buf = nameHost.getBytes();
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
+            try {
+                socket.send(packet);
+            } catch (IOException ex) {
+                Logger.getLogger(RobotController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+
+
+    private class Receiver extends ReceiverConnection{
+        @Override
+        public void doReceiverConnection(int port) throws SocketException{
+            try {
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                String received= new String(packet.getData(), 0, packet.getLength());
+                System.out.println(received);
+            } catch (IOException ex) {
+                Logger.getLogger(RobotController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+
 }
