758e8c67eb155b1af5d5cc54ebbe77a3ce7923bf
Miguel Dias <m3k3r1@gmail.com>
2018-06-05 17:44:14 +0200

    Broker Functions

--- src/middleware/Broker.java
+++ src/middleware/Broker.java
@@ -1,5 +1,46 @@
 package middleware;
 
-public class Broker
+import connection.ReceiverConnection;
+import connection.SenderConnection;
+
+public class Broker extends SenderConnection
 {
+    public Broker() {
+        new Thread(new HorizontalSkeletonListener()).start();
+        new Thread(new HorizontalStubListener()).start();
+        new Thread(new VerticalStubListener()).start();
+        new Thread(new VerticalSkeletonListener()).start();
+        new Thread(new GrabberSkeletonListener()).start();
+        new Thread(new GrabberStubListener()).start();
+        new Thread(new NameServerLisntener()).start();
+    }
+
+    private class HorizontalStubListener extends ReceiverConnection {
+
+    }
+    private class VerticalStubListener extends ReceiverConnection {
+
+    }
+    private class GrabberStubListener extends ReceiverConnection {
+
+    }
+    private class HorizontalSkeletonListener extends ReceiverConnection {
+
+    }
+    private class VerticalSkeletonListener extends ReceiverConnection {
+
+    }
+    private class GrabberSkeletonListener extends ReceiverConnection {
+
+    }
+    private class NameServerLisntener extends ReceiverConnection {
+
+    }
+
+    public void saveMessageToFile(){ }
+    public void checkFile(){}
+    
+    public static void main(String[] args){
+        new Broker();
+    }
 }
